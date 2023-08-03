ce30db2a1f070b13af219ea4e4bf867701f40b8e
Thaís Breder <thais.breder@zup.com.br>
2023-08-03 14:26:43 -0300

    :test_tube: test: corrige test orderDrink e orderSandwich

--- src/test/java/services/OrderServicesTest.java
+++ src/test/java/services/OrderServicesTest.java
@@ -1,15 +1,21 @@
 package services;
 
+import org.autoAtendimento.connection.Connection;
 import org.autoAtendimento.repositories.ClientRepository;
 import org.autoAtendimento.services.OrderServices;
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
 public class OrderServicesTest {
-
+    @Before
+    public void setUp() throws SQLException {
+        Connection.getConnection(); // Estabelece a conexão antes de iniciar o teste
+    }
     @Test
     public void testOrderSandwich() {
         // Cenário de teste
@@ -23,8 +29,7 @@ public class OrderServicesTest {
         OrderServices.orderSandwich(optionSandwich, sandwichQuantity, clientId);
 
         // Verifica se o pedido foi inserido corretamente no banco de dados de teste
-        // Você pode adicionar mais verificações aqui, dependendo do que deseja testar
-        // Neste caso, estamos verificando se o preço final do pedido é calculado corretamente.
+        // Verifica se o preço final do pedido é calculado corretamente.
         double expectedFinalPrice = 2 * 10.00; // Quantidade * Preço do X-burger
         double actualFinalPrice = OrderServices.getTotalOrderPrice(clientId);
         assertEquals(expectedFinalPrice, actualFinalPrice, 0.01);
